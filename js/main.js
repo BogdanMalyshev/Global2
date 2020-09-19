30683192ff81c801dafb3652fa84079e2f9a9aaf
Bogdan <bogdan.malyshev.art@gmail.com>
2020-09-19 13:17:12 +0200

    new animate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
-$(document).ready(function() {
-    $('.burger__menu').click(function(event) {
-      	$('.burger__menu,.main') .toggleClass('active');
-      	$('body') .toggleClass('lock');
-    });
+
+
+const burgerMenu = document.querySelector('.burger__menu'),
+	  main = document.querySelector('.main');
+
+burgerMenu.addEventListener('click', function (){
+	main.classList.toggle('active');
+	burgerMenu.classList.toggle('active');
 });
+// animate-----------------------------------------
+
+let animItems = document.querySelectorAll('.anim_items');
+
+ if (animItems.length > 0) {
+ 	window.addEventListener('scroll', animScrollOn);
+ 	function animScrollOn() {
+ 		animItems.forEach(e => {
+ 			const animItemsHeight = e.offsetHeight,
+ 				  animItemsOffset = offset(e).top,
+ 				  offsetHeight = document.documentElement.clientHeight;
+
+
+ 			if (pageYOffset + offsetHeight - (animItemsHeight / 4) >= animItemsOffset && pageYOffset < (animItemsOffset + animItemsHeight)) {
+ 				e.classList.add('active');
+ 			} else {
+ 				 e.classList.remove('active');
+ 			}
+ 		});
+ 	}
+
+ 	function offset (el) {
+ 		const coordinates = el.getBoundingClientRect(),
+ 			  scrollTop = window.pageXOffset || document.documentElement.scrollTop,
+ 			  scrollLeft = window.pageYOffset || document.documentElement.scrollLeft;
+
+ 		return {top: coordinates.top + scrollTop, left: coordinates.left + scrollLeft};
+ 	}
+
+
+ 	setTimeout(()=>animScrollOn(), 500);
+ }
